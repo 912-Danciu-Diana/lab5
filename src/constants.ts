173262eb50ef67d48eb93e8ddb188b29950b0c93
912-Danciu-Diana <91667594+912-Danciu-Diana@users.noreply.github.com>
2023-04-30 11:36:49 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "https://ec2-13-48-3-158.eu-north-1.compute.amazonaws.com:8000/api";
+const PROD_BACKEND_API_URL = "https://ec2-13-50-246-102.eu-north-1.compute.amazonaws.com:8000/api";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/api";
 
 export const BACKEND_API_URL =
